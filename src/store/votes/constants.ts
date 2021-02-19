117253fb9bf5df64ee2a6166411a0aa291f6d002
Matyas Gabor <matyasgabor.1987@gmail.com>
2021-02-19 22:18:52 +0100

    chore: handle the fetch routes with template literals and fix the button's styling error

--- src/store/votes/constants.ts
+++ src/store/votes/constants.ts
@@ -1,2 +1,6 @@
-export const fetchCurrentVotesURL: string = "http://localhost:5000";
-export const postVotesURL: string = "http://localhost:5000/vote";
+
+export const ROOT_DOMAIN: string = 'http://localhost:5000';
+export const VOTE_SUBROUTE: string = 'vote';
+/* you should use the vote subroute to get the current state of votes from backend */
+// export const fetchCurrentVotesURL: string = 'http://localhost:5000/vote';
+
