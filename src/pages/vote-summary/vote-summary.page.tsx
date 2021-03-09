f9ed5464619a6402de0f2084964df202a17b9c33
Matyas Gabor <matyasgabor.1987@gmail.com>
2021-03-09 22:52:41 +0100

    style: add background to vote-summary-page

--- src/pages/vote-summary/vote-summary.page.tsx
+++ src/pages/vote-summary/vote-summary.page.tsx
@@ -1,45 +1,44 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { fetchVotesThunk, resetVotesThunk } from '@store/votes/votes.thunk';
+import { fetchVotesThunk } from '@store/votes/votes.thunk';
 import Header from '@app/components/header/header';
 import VoteTable from '@components/vote-summary/vote-table/vote-table';
 import { selectVoteState } from '@store/votes/votes.selectors';
 import { selectUserState } from '@store/auth/auth.selectors';
-import { Roles } from '@app/store/auth/auth.constants';
-import { getToken } from '@store/auth/auth.utils';
-import { ResetVoteObject } from '@store/votes/votes.model';
+// import { Roles } from '@app/store/auth/auth.constants';
+// import { getToken } from '@store/auth/auth.utils';
+// import { ResetVoteObject } from '@store/votes/votes.model';
+import './vote-summary.page.scss'
 
 const VoteSummary = () => {
     const dispatch = useDispatch();
     const votes = useSelector(selectVoteState);
-    const currentUser = useSelector(selectUserState);
+    // const currentUser = useSelector(selectUserState);
 
     useEffect(() => {
         dispatch(fetchVotesThunk());
     }, [dispatch]);
 
-    const objectForSendingReset: ResetVoteObject = {
-        token: getToken(),
-        body: {
-            userId: currentUser.id.toString()
-        }
-    }
+    // const objectForSendingReset: ResetVoteObject = {
+    //     token: getToken(),
+    //     body: {
+    //         userId: currentUser.id.toString()
+    //     }
+    // }
 
-    const resetAllVoteFromSummaryThisWeek = (objectForSendingReset: ResetVoteObject) => {
-        dispatch(resetVotesThunk(objectForSendingReset));
-    }
+    // const resetAllVoteFromSummaryThisWeek = (objectForSendingReset: ResetVoteObject) => {
+    //     dispatch(resetVotesThunk(objectForSendingReset));
+    // }
 
     return (
         <>
-            <Header />
-            <VoteTable votes={votes.votes} />
-            { currentUser.role === Roles.Admin &&
-                <button
-                    className='vote-table-reset-button'
-                    onClick={() => resetAllVoteFromSummaryThisWeek(objectForSendingReset)} >Teljes törlés
-                </button>
-            }
+            <div className="votepage-background-picture">
+                <div className="background-gradient">
+                    <Header />
+                    <VoteTable votes={votes.votes} />
+                </div>
+            </div>
         </>
     )
 }
