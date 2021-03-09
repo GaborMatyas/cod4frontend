50e1ee2b823f0a77c9949b34346578d1148ae7d4
Matyas Gabor <matyasgabor.1987@gmail.com>
2021-03-09 23:04:18 +0100

    feat: create constant props for authPage

--- src/pages/login/auth.page.constants.ts
+++ src/pages/login/auth.page.constants.ts
@@ -1,15 +1,20 @@
 import { FormTypes } from '@components/form/constants';
+import { authPageProps } from './auth.page';
 
-export const LoginProps = {
+export const LoginProps: authPageProps = {
     title: 'Bejelentkezés',
     formClass: FormTypes.LOGIN_PAGE,
     question: 'Még nem vagy besorozva?',
-    smallButtonText: 'Regisztrálj'
+    smallButtonText: 'Regisztrálj',
+    headline: 'Üdv újra itt! Jelentkezz be és szavazz mikor legyen a legközelebbi öldöklés!',
+    instruction: 'Ha elfelejtetted a jelszavadat vagy törölnéd a profilodat, lépj kapcsolatba Plato-val!'
 }
 
-export const RegistrationProps = {
+export const RegistrationProps: authPageProps = {
     title: 'Regisztráció',
     formClass: FormTypes.REGISTER_PAGE,
     question: 'Már be vagy sorozva?',
-    smallButtonText: 'Jelentkezz be!'
+    smallButtonText: 'Jelentkezz be!',
+    headline: 'Gyülekező! Regisztráld magad a CoD 4 Légióba és nyerj egy ingyen utat a pokolba!',
+    instruction: 'Ne adj meg olyan jelszót amit máshol is használsz, így is nyomunkban az FBI'
 }
--- src/pages/login/auth.page.tsx
+++ src/pages/login/auth.page.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import Form from '@components/form/form';
-import { ROUTE_PATHS } from '@router/routes';
 import { FormTypes } from '@components/form/constants';
 import { LoginProps, RegistrationProps } from './auth.page.constants';
 import { selectAppLoading } from '@store/app/app.selectors';
@@ -12,11 +11,13 @@ import { useSelector } from 'react-redux';
 
 import './auth.page.scss';
 
-type Props = {
+export type authPageProps = {
     title: string;
     formClass: FormTypes;
     question: string;
     smallButtonText: string;
+    headline: string;
+    instruction: string;
 }
 
 const AuthPage = () => {
@@ -24,28 +25,35 @@ const AuthPage = () => {
     const [password, setPassword] = useState('');
     const isAppLoading = useSelector(selectAppLoading);
     const [isLoginPage, setLoginpage] = useState(true);
-    const props: Props = isLoginPage ? LoginProps : RegistrationProps;
+    const props: authPageProps = isLoginPage ? LoginProps : RegistrationProps;
 
     return (
         <>
-            <div className="container-background">
-                {isAppLoading ? <Loader /> : null}
-                <div className="login-container">
-                    <div className="title-container">
-                        <Logo />
-                        <h1 className="title">{props.title}</h1>
-                    </div>
+            <div className="authpage-background-picture">
+                <div className="background-gradient">
+                    <div className="auth-page-big-logo" />
+                    <div className="login-container-background">
+                        {isAppLoading ? <Loader /> : null}
+                        <div className="login-container">
+                            <div className="title-container">
+                                <Logo />
+                                <h3 className="headline">{props.headline}</h3>
+                                <span className="instruction">{props.instruction}</span>
+                                <h2 className="form-title">{props.title}</h2>
+                            </div>
 
-                    <Form
-                        classNameString={props.formClass}
-                        username={email}
-                        setUsername={setUsername}
-                        password={password}
-                        setPassword={setPassword} />
+                            <Form
+                                classNameString={props.formClass}
+                                username={email}
+                                setUsername={setUsername}
+                                password={password}
+                                setPassword={setPassword} />
 
-                    <div className="change-page">
-                        <span className="question">{props.question}</span>
-                        <button onClick={() => setLoginpage(!isLoginPage)}>{props.smallButtonText}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&gt;</button>
+                            <div className="change-page">
+                                <span className="question">{props.question}</span>
+                                <button onClick={() => setLoginpage(!isLoginPage)}>{props.smallButtonText}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&gt;</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
